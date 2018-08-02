ad6b831efd755fd368cd130701e02c188c30ba94
chenjinhui520 <lch@313748>
2018-08-02 11:45:30 +0800

    文件批量重命名工具

---
+++ file_batch_rename.py
@@ -0,0 +1,22 @@
+import os
+
+
+p = 'D:' + os.sep + 'get' + os.sep + 'SS' + os.sep + 'views'
+
+for parent, dirname, filenames in os.walk(p):
+    # parent：父目录
+    # dirname：父目录下面的子目录
+    # filenames：父目录或子目录下面的所有文件，一个目录下面的文件构成一行列表
+    for filename in filenames:
+        # 将文件名切割成前缀和后缀
+        prefix, suffix = filename.split('.')
+        # 获取旧的文件名
+        old_file_name = prefix + '.' + suffix
+        # 获取新的文件名
+        new_file_name = prefix + '.html'
+        # 获取旧的文件绝对路径
+        old_name = os.path.join(parent, old_file_name)
+        # 获取新的文件绝对路径
+        new_name = os.path.join(parent, new_file_name)
+        # 重命名文件
+        os.rename(old_name, new_name)
